7f49b2cc1d798ddf521c68dcc8b9e6507f1ec781
Arnold Rozon <arnold@rozon.org>
2021-04-10 04:03:00 -0500

    Cleaned up the ClassifierViewController work and displays model output in table cells

--- Trendeye/Controllers/ClassifierViewController.swift
+++ Trendeye/Controllers/ClassifierViewController.swift
@@ -32,18 +32,10 @@ final class ClassifierViewController: UITableViewController, TEClassifierDelegat
         applyConfigurations()
         removePreviousViewController()
     }
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
         tableView.register(ClassifierResultCell.self, forCellReuseIdentifier: ClassifierResultCell.reuseIdentifier)
-        tableView.backgroundColor = .systemIndigo
-        
         classifier.delegate = self
         beginClassification(of: photo)
     }
@@ -86,86 +78,32 @@ final class ClassifierViewController: UITableViewController, TEClassifierDelegat
         guard let image = CIImage(image: photo) else { return }
         classifier.classifyImage(image)
     }
-
+    
     // MARK: - Table view data source
-
+    
     override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
         return 1
     }
-
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 100
+        return results?.count ?? 0
     }
-
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: ClassifierResultCell.reuseIdentifier, for: indexPath)
-        cell.textLabel?.text = "\(indexPath.row)"
-
+        let result = results?[indexPath.row]
+        cell.textLabel?.text = "\(result!.identifier) /// \(result!.confidence)"
         return cell
     }
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-        
+    
     // MARK: - TEClassifierDelegate
     
     func didFinishClassifying(_ sender: TEClassifierManager?, results: [VNClassificationObservation]) {
-        print("Completed image classification!")
-        
         if !results.isEmpty {
             self.results = results
-            results.forEach({
-                print("Identifier: \($0.identifier)   ||   Confidence: \($0.confidence)")
-            })
         } else {
             presentAlert(title: "Oh no", message: "Seems like something went wrong on our end. Sorry about that.", actionTitle: "Try again later")
         }
-        
-        // TODO: Update view controller to present results
     }
     
     func didError(_ sender: TEClassifierManager?, error: Error?) {
@@ -180,5 +118,5 @@ final class ClassifierViewController: UITableViewController, TEClassifierDelegat
             }
         }
     }
-
+    
 }
